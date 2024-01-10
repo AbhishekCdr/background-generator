64604120e487db28473646168e30d0b044ee7d84
AbhsihekCdr <abhikr.ak.2001@gmail.com>
2024-01-10 13:41:01 +0530

    adding clip to copy

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ function setGradient() {
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+
+function copyToClipboard() {
+    var copyText = document.getElementById("contentToCopy").innerText;
+    var textArea = document.createElement("textarea");
+
+    textArea.value = copyText;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+    document.body.removeChild(textArea);
+
+    // Show the copied message
+    var copyMessage = document.getElementById("copyMessage");
+    copyMessage.style.display = "inline";
+
+    // Hide the message after 2 seconds (you can adjust the time)
+    setTimeout(function() {
+        copyMessage.style.display = "none";
+    }, 2000);
+}
